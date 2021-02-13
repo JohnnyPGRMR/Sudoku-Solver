68cf42249df26c1cd306cc47922a8d4964d7a0db
JohnnyPGRMR <76419684+JohnnyPGRMR@users.noreply.github.com>
2021-02-12 18:48:27 -0800

    Update Solver.py

--- Solver.py
+++ Solver.py
@@ -2,35 +2,44 @@
 
 # Here is an example sudoku board I will be using for the moment hard coded in.
 board = [
-        [0,0,0,0,6,0,1,0,0],
-        [0,0,4,0,9,5,0,6,8],
-        [0,6,1,0,7,0,9,3,0],
-        [0,5,0,0,0,0,4,0,7],
-        [1,0,0,4,2,9,0,0,6],
-        [4,0,6,0,0,0,0,1,0],
-        [0,2,5,0,3,0,7,4,0],
-        [6,1,0,7,4,0,5,0,0],
-        [0,0,9,0,8,0,0,0,0],
+    [0,0,0,0,6,0,1,0,0],
+    [0,0,4,0,9,5,0,6,8],
+    [0,6,1,0,7,0,9,3,0],
+    [0,5,0,0,0,0,4,0,7],
+    [1,0,0,4,2,9,0,0,6],
+    [4,0,6,0,0,0,0,1,0],
+    [0,2,5,0,3,0,7,4,0],
+    [6,1,0,7,4,0,5,0,0],
+    [0,0,9,0,8,0,0,0,0],
 ]
-print(board)
+def plot_board(bo):
+    ''' Draws a sudoku board in the terminal window.
+    Input parameter bo is the 2d array of board values.
+    :return: None.'''
 
-
-def print_board(bo):
-    """
-    prints the board
-    :param bo: 2d List of ints
-    :return: None
-    """
+    # Where i represents the index of the row of values (board after all is a 2d array of numbers).
     for i in range(len(bo)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - -")
-        for j in range(len(bo[0])):
-            if j % 3 == 0:
+        # This is more of a cosmetic choice, 
+        # the i != 0 condition changes where the first seperation line gets drawn.
+        if i % 3 == 0 and i != 0:  
+            print("- - - - - - - - - - - -")
+
+        # Where j represents the index of the column of values.  
+        # Since it is an N x N matrix either 0 or i could be put in the for the index of the variable "bo".
+        for j in range(len(bo[i])):
+            # Cosmetic choice, but is the simplest way to draw a board, 
+            # changing it will require more lines of code to completely draw a symmetric board.
+            if j % 3 == 0 and j != 0:  #
+                # Print has an additional parameter "end" that by default does a carriage return, 
+                # change to "" so the next print command plots on the same line.
                 print(" | ",end="")
 
+            # If confused and thinking should check for j == 9 remember that python starts indexing at 0.  
+            # So 8 is the position of the 9th number!
             if j == 8:
-                print(bo[i][j], end="\n")
+                print(bo[i][j])
+            # The else statement below is what actually prints the numbers in the board.
             else:
                 print(str(bo[i][j]) + " ", end="")
 
-print_board(board)
+plot_board(board)
